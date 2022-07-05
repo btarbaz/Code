7bb3660fa50a2834288ecbc7ef4369568b11108b
lappichu <btarbazz@gmail.com>
2022-07-05 13:52:19 +0500

    updated on 05072022

--- restApi/script.js
+++ restApi/script.js
@@ -1,11 +1,68 @@
-const getJson = function (url, action = '') {
-  return fetch(url, {
-    method: action,
-  }).then(res => res.json());
+const myform = document.querySelector('#form-submit');
+const userList = document.querySelector('#user-list');
+//User block
+const getUser = async function () {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/users`);
+  console.log(res);
+  const dataUsers = await res.json();
+  console.log(dataUsers);
+  displayUsers(dataUsers);
 };
+getUser();
 
-const msgTodos = document.querySelector('.msg');
-getJson('https://jsonplaceholder.typicode.com/todos/1', 'GET').then(data => {
-  console.log(data);
-  msgTodos.textContent = JSON.stringify(data);
+//UI Block
+const displayUsers = function (dataApi) {
+  users = dataApi;
+  users.forEach(user => addUserToList(user));
+};
+const addUserToList = function (data) {
+  const userList = document.querySelector('#user-list');
+  const row = document.createElement('tr');
+  row.innerHTML = `<td>${data.username}</td><td>${data.name}</td><td>|</td><td><a href="" class="btn-edit">Edit</a></td><td><a href="#" class="btn-delete">X</a></td>`;
+  userList.appendChild(row);
+};
+//clear fields
+const clearFields = function () {
+  document.querySelector('#username').value = '';
+  document.querySelector('#user').value = '';
+};
+//Delete user
+const userDelete = function (el) {
+  if ((el.textNode = 'X')) {
+    el.parentElement.parentElement.remove();
+  }
+};
+//Event
+userList.addEventListener('click', dl => {
+  userDelete(dl.target);
+});
+
+//Edit user
+//Event
+userList.addEventListener('click', edit => {
+  edit.preventDefault();
+  console.log(edit.target);
+});
+
+//Add user
+//Event
+
+myform.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  const username = document.querySelector('#username').value;
+  const user = document.querySelector('#user').value;
+
+  if (username === '' || user === '') {
+    const msg = document.querySelector('.msg');
+    msg.innerHTML = 'Invalid inputs';
+    setTimeout(() => {
+      msg.innerHTML = '';
+    }, 3000);
+  } else {
+    userProfile = { username: username, name: user };
+    console.log(userProfile);
+    addUserToList(userProfile);
+    clearFields();
+  }
 });
